83518e280d3383cadf56f0146b15120ee7a46bfe
Bahari1161 <eduardojimenez1161@gmail.com>
2024-01-07 19:10:12 +0100

    miaus 20000

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-console.log('Hello!');
 
-let hsdjnisd = 'jnkwdco';
-
-for (let index = 0; index < 10; index++) {
-
-  console.log('gay', index);
-}
-
-
-const monoDiv = document.getElementById('mono')
-
-
-
-const body = document.getElementsByTagName('body')
-
-
-console.log(document.body.innerText) 
 
 const fondo = document.getElementById('miau')
-
-
 
 for (let index = 0; index < 6; index++) {
   fondo.innerText = 'miau' 
   
 }
+
+let currentIndex = 0;
+const carouselContainer = document.getElementsByClassName('.carousel-container');
+const totalSlides = document.getElementsByClassName('carousel-item')
+
+console.log("hey2",totalSlides)
+
