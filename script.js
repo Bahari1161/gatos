9487b786cc7d92be701daaf4ecb74cbffb2f3264
Bahari1161 <eduardojimenez1161@gmail.com>
2024-01-07 18:42:28 +0100

    fondo de gatos

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ for (let index = 0; index < 10; index++) {
 
 
 const monoDiv = document.getElementById('mono')
+
+
+
+const body = document.getElementsByTagName('body')
+
+
+console.log(document.body.innerText) 
+
+const fondo = document.getElementById('miau')
+
+
+
+for (let index = 0; index < 6; index++) {
+  fondo.innerText = 'miau' 
+  
+}
